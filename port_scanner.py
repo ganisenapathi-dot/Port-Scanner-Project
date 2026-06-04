c6f2236503e909d6a3f35ec5ab562e708bc07090
ganisenapathi-dot <ganisenapathi@gmail.com>
2026-06-04 10:25:50 +0530

    Create port_scanner.py

---
+++ port_scanner.py
@@ -0,0 +1,20 @@
+import socket
+
+target = input("Enter IP Address or Domain: ")
+
+print(f"\nScanning {target}...\n")
+
+ports = [21, 22, 23, 25, 53, 80, 110, 143, 443]
+
+for port in ports:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+
+    result = sock.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} is OPEN")
+    else:
+        print(f"Port {port} is CLOSED")
+
+    sock.close()
